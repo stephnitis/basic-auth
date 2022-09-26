de47493e8c1c35af8a0445f9d2d1220c43156b59
Stephanie Hill <stephni.hill@gmail.com>
2022-09-26 12:10:50 -0700

    more dependencies

---
+++ src/auth/middleware/basic.js
@@ -0,0 +1,11 @@
+
+
+// async function basicAuth(req, res, next){
+//   let {authorization} = req.headers;
+//   console.log('authorization');
+//   if(!authorization){
+//     res.status(401).send('Not Authorized');
+//   } else {
+//     let authString = authorization
+//   }
+// }
